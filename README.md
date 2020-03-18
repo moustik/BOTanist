69530a332f50db19c6b0b72392ae9d32d80e1799
moustik <damien.letournel@gmail.com>
2020-03-18 09:24:31 +0100

    boilerplate for echo bot

--- README.md
+++ README.md
@@ -2,7 +2,18 @@
 
 ## Conan
 
+### Install conan
+
+See https://docs.conan.io/en/latest/installation.html
+
 ### Add new remotes
 
 conan remote add community https://api.bintray.com/conan/conan-community/conan
 conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
+
+# Build
+
+```sh
+mkdir build; cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+```
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,57 @@
 #include <iostream>
+
+#include <csignal>
+#include <cstdio>
+#include <cstdlib>
+#include <exception>
+#include <string>
+
 #include <boost/version.hpp>
 
-int main(void) {
+#include <tgbot/tgbot.h>
+
+using namespace std;
+using namespace TgBot;
+
+int main() {
+
     std::cout << "Using Boost " << BOOST_VERSION / 100000 << "."
                    << BOOST_VERSION / 100 % 1000 << "."
                    << BOOST_VERSION % 100 << std::endl;
+
+    string token(getenv("TOKEN"));
+    printf("Token: %s\n", token.c_str());
+
+    Bot bot(token);
+    bot.getEvents().onCommand("start", [&bot](Message::Ptr message) {
+        bot.getApi().sendMessage(message->chat->id, "Hi!");
+    });
+    bot.getEvents().onAnyMessage([&bot](Message::Ptr message) {
+        printf("User wrote %s\n", message->text.c_str());
+        if (StringTools::startsWith(message->text, "/start")) {
+            return;
+        }
+        bot.getApi().sendMessage(message->chat->id, "Your message is: " + message->text);
+    });
+
+    signal(SIGINT, [](int s) {
+        printf("SIGINT got\n");
+        exit(0);
+    });
+
+    try {
+        printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
+        bot.getApi().deleteWebhook();
+
+        TgLongPoll longPoll(bot);
+        while (true) {
+            printf("Long poll started\n");
+            longPoll.start();
+        }
+    } catch (exception& e) {
+        printf("error: %s\n", e.what());
+    }
+
+    return 0;
+
 }
